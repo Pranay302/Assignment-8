da5abcc94026830f70caaa018c0b8b6d6b969fe2
Pranay302 <mhatrepranay60@gmail.com>
2019-10-20 11:15:09 -0700

    Assignment-8
    javascript code updated

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,7 +1,8 @@
 /*eslint-env browser*/
-var submit = document.getElementById("submit");
+window.onload = load_employee_list;
+var submit = document.getElementById("submit_btn");
 submit.onclick=add_new_employee;
-//submit.onclick=clear_textbox;
+//preexisting list of employees that are present on loading the page
 var employee_list = [
 		["Pranay Mhatre", "CEO", 100],
 		["Gaurav Burange", "CTO", 101],
@@ -9,51 +10,53 @@ var employee_list = [
 		["Akash More", "Team Lead", 103],
 		["Tanmay Bhawalkar", "HR", 104],
 ]
-
+//function to clear out all the textbox values entered.
 function clear_textbox(){
     document.getElementById("form_name").value="";
     document.getElementById("form_title").value="";
     document.getElementById("form_extension").value="";
 }
-function load_employee_list() {
-	var html = ""
-	for (var i = 0; i < employee_list.length; i++) {
-		var employee = employee_list[i];
-		html += '<tr><td>' + employee[0] + '</td><td>' + employee[1] + '</td><td>' + employee[2] + 
-		'</td><td><button onclick="delete_emp(' + i + ')">Delete</button></td></tr>';
-	}
-	document.querySelector("tbody").innerHTML = html;
-	document.getElementById("count_of_emp").innerHTML = employee_list.length;
-}
-
+//function that is triggered on clicking the delete button
 function delete_emp(n) {
 	employee_list.splice(n, 1);
 	load_employee_list();
 }
-
+//this function generates the required html to display the table of employee
+function load_employee_list() {
+	var html_code = ""
+	for (var i = 0; i < employee_list.length; i++) {
+		var employee = employee_list[i];
+		html_code += '<tr><td>' + employee[0] + '</td><td>' + employee[1] + '</td><td>' + employee[2] + 
+		'</td><td><button onclick="delete_emp(' + i + ')">Delete</button></td></tr>';
+	}
+	document.querySelector("tbody").innerHTML = html_code;
+	document.getElementById("count_of_emp").innerHTML = employee_list.length;
+}
+//pushes the newly entered values to the array of employee. also Writes a message to the span tag if the data in textbox is empty
 function add_new_employee() {
 	var emp_name = document.getElementById("form_name").value;
 	var emp_title = document.getElementById("form_title").value;
 	var emp_extension = document.getElementById("form_extension").value;
-	if (emp_name.length==0 || emp_title.length==0 || emp_extension.length==0){
-        if(emp_name.length==0){
-            window.alert("Employee name is empty.");
-            return;
-        }else if(emp_title.length==0){
-            window.alert("Employee title is empty.");
-            return;
-        }else if(emp_extension.length==0){
-            window.alert("Employee extension is empty.");
-            return;
-        }
-        
-    } 
-	employee_list.push([emp_name, emp_title, emp_extension]);
+	
+    if(emp_name.length==0){
+		document.getElementById("form_name_span").innerHTML = "Employee name is empty.";
+        return;
+	}else{
+		document.getElementById("form_name_span").innerHTML = "";
+	}
+	if(emp_title.length==0){
+        document.getElementById("form_title_span").innerHTML = "Employee title is empty.";
+        return;
+	}else{
+		document.getElementById("form_title_span").innerHTML = "";
+	}
+	if(emp_extension.length==0){
+        document.getElementById("form_extension_span").innerHTML = "Employee extension is empty.";
+        return;
+    }else{
+		document.getElementById("form_extension_span").innerHTML = "";
+	}
+    employee_list.push([emp_name, emp_title, emp_extension]);
     load_employee_list();
     clear_textbox();
-}
-
-
-window.onload = function () {
-	load_employee_list();
 }
